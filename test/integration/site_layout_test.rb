c94548e83c572f071cc3982ab8f9d75daeedbfb0
Kenji Kato <k.524.10nis@gmail.com>
2018-01-28 11:45:20 +0000

    Implement basic login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,6 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-    assert_select "title", full_title("Contact")
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    #assert_select "title", full_title("Contact")
   end
 end
